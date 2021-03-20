accf934235c9caf68b5d323bf1712a92bc4dd97d
Javier <javier.rb_96@hotmail.com>
2021-03-20 17:13:20 +0100

    Add publish method and script

--- bin/gh_pages.dart
+++ bin/gh_pages.dart
@@ -1,6 +1,6 @@
 import 'package:gh_pages/gh_pages.dart';
 
-void main() {
+void main(List<String> arguments) {
   final ghpages = GitHubPages();
   ghpages.publish();
 }
--- lib/src/gh_pages_base.dart
+++ lib/src/gh_pages_base.dart
@@ -1,6 +1,17 @@
-// TODO: Put public facing types in this file.
+import 'dart:io';
 
-/// Checks if you are awesome. Spoiler: you are.
-class Awesome {
-  bool get isAwesome => true;
+import 'package:git/git.dart';
+import 'package:path/path.dart' as p;
+
+class GitHubPages {
+  Future<void> publish() async {
+    try {
+      final gitDir = await GitDir.fromExisting(p.current);
+      await gitDir.updateBranchWithDirectoryContents('gh-pages', p.join(p.current, 'build', 'web'), 'Updates');
+      await gitDir.runCommand(['push', 'origin', 'gh-pages']);
+      print('Published');
+    } on ProcessException catch (e) {
+      print(e);
+    }
+  }
 }
